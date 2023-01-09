b5b711fea0782dabab17acd2d6aabf9542d9782d
Jefferson <jefferson.leal@7comm.com.br>
2023-01-09 16:33:14 -0300

    Implementando o método busca por id

--- src/main/java/br/com/java/jdbc/dao/UsuarioDAO.java
+++ src/main/java/br/com/java/jdbc/dao/UsuarioDAO.java
@@ -2,7 +2,10 @@ package br.com.java.jdbc.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.java.jdbc.conexao.SingleConnection;
 import br.com.java.jdbc.model.Usuario;
@@ -33,5 +36,66 @@ public class UsuarioDAO {
 			}
 			e.printStackTrace();
 		}
+	}
+
+	public List<Usuario> ListAll() {
+		List<Usuario> list = new ArrayList<Usuario>();
+
+		String sql = "select * from usuariojava";
+
+		try {
+			PreparedStatement statement = connection.prepareStatement(sql);
+			ResultSet resultado = statement.executeQuery();
+
+			while (resultado.next()) {
+				Usuario usuario = new Usuario();
+				usuario.setId(resultado.getLong("id"));
+				usuario.setNome(resultado.getString("nome"));
+				usuario.setEmail(resultado.getString("email"));
+				list.add(usuario);
+			}
+
+			return list;
+		} catch (SQLException e) {
+			try {
+				connection.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}
+		
+		return list;
+
+	}
+	
+	
+	public Usuario findById(long id) {
+		Usuario usuario = new Usuario();
+
+		String sql = "select * from usuariojava where id = " + id;
+
+		try {
+			PreparedStatement statement = connection.prepareStatement(sql);
+			ResultSet resultado = statement.executeQuery();
+
+			while (resultado.next()) {
+				
+				usuario.setId(resultado.getLong("id"));
+				usuario.setNome(resultado.getString("nome"));
+				usuario.setEmail(resultado.getString("email"));
+			}
+			
+		} catch (SQLException e) {
+			try {
+				connection.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}
+		
+		return usuario;
+
 	}
 }
--- src/test/java/java_jdbc/java_jdbc/TesteBancoJdbc.java
+++ src/test/java/java_jdbc/java_jdbc/TesteBancoJdbc.java
@@ -1,5 +1,8 @@
 package java_jdbc.java_jdbc;
 
+import java.util.Iterator;
+import java.util.List;
+
 import org.junit.Test;
 
 import br.com.java.jdbc.conexao.SingleConnection;
@@ -7,21 +10,51 @@ import br.com.java.jdbc.dao.UsuarioDAO;
 import br.com.java.jdbc.model.Usuario;
 
 public class TesteBancoJdbc {
-	
+
 	@Test
 	public void initBanco() {
 		SingleConnection.getConnection();
 	}
-	
+
 	@Test
 	public void insert() {
 		UsuarioDAO usuarioDAO = new UsuarioDAO();
 		Usuario usuario = new Usuario();
-		
+
 		usuario.setId(3L);
 		usuario.setNome("Arthur");
 		usuario.setEmail("Arthur@gmail.com");
+
+		usuarioDAO.insert(usuario);
+	}
+
+	@Test
+	public void listAll() {
+		UsuarioDAO dao = new UsuarioDAO();
+		try {
+			List<Usuario> list = dao.ListAll();
+			
+			for (Usuario usuario : list) {
+				System.out.println(usuario);
+				System.out.println("----------------------------------------------------------");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void finById() {
 		
-		usuarioDAO.insert(usuario);	
+		UsuarioDAO dao = new UsuarioDAO();
+		
+		try {
+			Usuario usuario = dao.findById(3L);	
+			
+			System.out.println(usuario);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
